17df6323e9887933eed6195961c2f15ef59982ae
Aayush Laddha <aayushladdha001@gmail.com>
2025-07-17 14:27:54 +0530

    .

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,16 +1,55 @@
-import React from 'react'
+import { useState } from 'react'
 import Navbar from '../components/Navbar'
 
 import Footer from '../components/Footer'
 
 const Events = () => {
+
+  const [isSubmitting, setisSubmitting] = useState(false)
+  const onSubmitHandler = (e) => {
+    setisSubmitting(true)
+    e.preventDefault()
+    setTimeout(() => {
+      setisSubmitting(false)
+    }, 2000);
+  }
+
   return (
     <>
-    <Navbar />
-    <div>
-      
-    </div>
-    <Footer />
+      <Navbar />
+      <div className='p-5'>
+        {/* search section */}
+        <form onSubmit={onSubmitHandler} className='flex flex-col gap-5'>
+          <div className='flex flex-col md:flex-row gap-5'>
+            <section className='flex flex-col gap-3 md:w-1/3'>
+              <label htmlFor="text">Search</label>
+              <input required className='p-3 py-1 rounded-lg ring ring-offset-4 ring-blue-500' type="text" name="text" id="text" />
+            </section>
+
+            <section className='flex flex-col gap-3 md:w-1/3'>
+              <label htmlFor="category">Category</label>
+              <select className='p-3 py-1.5 rounded-lg ring ring-offset-4 ring-blue-500' name="category" id="category">
+                <option value="aaa">aaa</option>
+                <option value="bbb">bbb</option>
+                <option value="ccc">ccc</option>
+              </select>
+            </section>
+
+            <section className='flex flex-col gap-3 md:w-1/3'>
+              <label htmlFor="date">Date</label>
+              <input className='p-3 py-1 rounded-lg ring ring-offset-4 ring-blue-500' type="date" name="date" id="date" />
+            </section>
+          </div>
+
+          <button type="submit"
+            disabled={isSubmitting}
+            className={`font-bold rounded-xl p-3 py-1 w-full md:w-fit m-auto ${isSubmitting ? 'text-black cursor-not-allowed' : 'text-white bg-blue-500 hover:bg-blue-600 cursor-pointer'}`}
+          >
+            {isSubmitting ? 'Searching...' : 'Search'}
+          </button>
+        </form>
+      </div>
+      <Footer />
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,14 @@ const Home = () => {
         <>
             <Navbar />
             <div>
+                <section className='flex flex-col items-center justify-center h-screen'>
+                    <h1 className='text-4xl font-bold'>Welcome to Eventiva</h1>
+                    <p className='text-lg mt-4'>Discover the best events and experiences in your city</p>
+                </section>
 
+                <section className='flex flex-col items-center justify-center h-screen bg-black'>
+
+                </section>
             </div>
             <Footer />
         </>
