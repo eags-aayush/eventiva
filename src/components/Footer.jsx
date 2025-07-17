2ccf0f3aeb789228bd308cb16ac0e06c054225fa
Aayush Laddha <aayushladdha001@gmail.com>
2025-07-17 16:00:42 +0530

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className='text-sm text-gray-500 font-bold text-center p-3 border-t border-gray-300'>
             <p>This is a demo website created for project demonstration purposes only. All content is fictional and does not represent real-life scenarios, entities, or individuals.</p>
             <p>All images used on this website are sourced from Google and remain the property of their respective copyright owners.</p>
-            <span>© 2025 Enentiva. All rights reserved.</span>
+            <span>© 2025 Enentivate. All rights reserved.</span>
         </footer>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav>
       <ul className='sticky top-0 flex justify-between items-center px-5 p-3'>
-        <span className='text-xl font-bold cursor-default'>Eventiva</span>
+        <span className='text-xl font-bold cursor-default'>Eventivate</span>
         <Link to='/' className='font-light hover:font-medium flex items-center'><Home className='w-10 h-5'/> Home</Link>
         <Link to='/events' className='font-light hover:font-medium flex items-center'><CalendarSearch className='w-10 h-5'/> Events</Link>
       </ul>
